b99dc8164b1dd25e78bfb6ecc86b9cf230c73483
MrNiNaD <ninadparkar273@gmail.com>
2025-06-22 12:31:50 +0530

    unnecessary files removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -762,11 +751,11 @@ export default function RootLayout({
             </div>
           </section>
         </main>
-        <script src="js/jquery.min.js"></script>
+        {/* <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
         <script src="js/jquery.sticky.js"></script>
         <script src="js/click-scroll.js"></script>
-        <script src="js/custom.js"></script>
+        <script src="js/custom.js"></script> */}
       </body>
     </html>
   );

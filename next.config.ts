fbf833ceee2af85afb7a6a03cff09e261c49cec5
MrNiNaD <ninadparkar273@gmail.com>
2025-07-12 11:03:08 +0530

    legacy browser support false

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    async headers() {
+  async headers() {
     return [
       {
-        source: '/images/:all*', // adjust path as needed
+        source: "/images/:all*", // adjust path as needed
         headers: [
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400, must-revalidate',
+            key: "Cache-Control",
+            value: "public, max-age=86400, must-revalidate",
           },
         ],
       },
     ];
   },
+  legacyBrowsers: false,
+  transpilePackages: [], // don't transpile everything unless needed
 };
 
 export default nextConfig;

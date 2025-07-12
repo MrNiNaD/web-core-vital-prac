03e8568addc22e064b2139c9cb35c9c791658445
MrNiNaD <ninadparkar273@gmail.com>
2025-07-12 10:24:31 +0530

    cache ttl added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async headers() {
+    return [
+      {
+        source: '/images/:all*', // adjust path as needed
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
